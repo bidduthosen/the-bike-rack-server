0c6f9c04fc2145c2adc46bb82c4a96c3ef239294
bidduthosen <mdbidduthosen34@gmail.com>
2022-12-07 18:57:37 +0600

    update now

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
+// jwt
 function verifyJWT(req, res, next){
     const authHeader = req.headers.authorization;
     if(!authHeader){
